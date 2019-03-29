b12716d9a3d83001f0747564d299d03edbc43371
Salam <shaiksalam9182@gmail.com>
2019-03-29 04:56:09 +0000

    temp Changes

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ helloModule.controller('hello', function($scope, $http, $cookies, $location) {
             $scope.message = "phone number is invalid"
             console.log('phone length is invalid');
         } else {
-            var url = "https://naaradh.in/login"
+            var url = "https://admin.naaradh.in/login"
             var data = {
                 phone: $scope.phone,
                 password: $scope.password,
@@ -122,7 +122,7 @@ helloModule.controller('postbody', function($scope, $http, $cookies, $state) {
         testin: 'testing'
     }
 
-    $http.post("https://naaradh.in/demo_post_limit", data).then(function(msg) {
+    $http.post("https://admin.naaradh.in/demo_post_limit", data).then(function(msg) {
         console.log(msg);
         if (msg.data.status == "success") {
             $scope.isLoading = false;
@@ -166,7 +166,7 @@ helloModule.controller('placebody', function($scope, $http, $cookies) {
         testin: 'testing'
     }
 
-    $http.post("https://naaradh.in/demo_place_limit", data).then(function(msg) {
+    $http.post("https://admin.naaradh.in/demo_place_limit", data).then(function(msg) {
         console.log(msg);
         if (msg.data.status == "success") {
             $scope.isLoading = false;
@@ -208,7 +208,7 @@ helloModule.controller('alienbody', function($scope, $cookies, $http) {
         testin: 'testing'
     }
 
-    $http.post("https://naaradh.in/demo_alien_limit", data).then(function(msg) {
+    $http.post("https://admin.naaradh.in/demo_alien_limit", data).then(function(msg) {
         console.log(msg);
         if (msg.data.status == "success") {
             $scope.isLoading = false;
@@ -248,7 +248,7 @@ helloModule.controller('moviebody', function($scope, $http, $cookies) {
         testin: 'testing'
     }
 
-    $http.post("https://naaradh.in/demo_movie_limit", data).then(function(msg) {
+    $http.post("https://admin.naaradh.in/demo_movie_limit", data).then(function(msg) {
         console.log(msg);
         if (msg.data.status == "success") {
             $scope.isLoading = false;
