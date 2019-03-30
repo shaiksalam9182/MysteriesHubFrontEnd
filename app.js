2fe1049bc19dedf2c16f1da3b7e836a8f2b869dd
shaiksalam1314 <salam.shaik@tv2z.com>
2019-03-30 17:14:40 +0530

    Added editor and working on register page

--- app.js
+++ app.js
@@ -55,6 +55,13 @@ helloModule.config(function($stateProvider, $urlRouterProvider) {
         controller: 'writerController'
     })
 
+    .state('Home.Register', {
+        name: "Register",
+        url: '/Register',
+        templateUrl: 'register.html',
+        controller: 'registerController',
+    })
+
 
     $stateprovideRef = $stateProvider;
 })
@@ -407,11 +414,27 @@ helloModule.controller('homeController', function($scope, $mdDialog, $cookies, $
         $scope.phone = $cookies.get("user");
         $scope.token = $cookies.get("token");
         console.log('cookiesdata', $scope.phone, $scope.token);
-        if (($scope.phone == "" || $scope.token == "") || ($scope.phone == undefined || $scope.token == undefined)) {
-            $location.path('/Login')
+        if ($scope.phone == "" || $scope.token == "") {
+            console.log('userNotLoggedIN');
+            $scope.showLoginOptions = true;
+            $scope.showUserOptions = false;
+            $scope.number = "2";
+            $mdMenu.open(ev);
         } else {
+            console.log('userLoggedIn');
+            $scope.showLoginOptions = false;
+            $scope.showUserOptions = true;
+            $scope.number = "4";
             $mdMenu.open(ev);
         }
+    }
+
+    $scope.goToLogin = function() {
+        $location.path("/Login");
+    }
+
+    $scope.goToRegister = function() {
+        $location.path("/Register");
     }
 
     $scope.logout = function() {
@@ -432,15 +455,105 @@ helloModule.controller('homeController', function($scope, $mdDialog, $cookies, $
 
 
 helloModule.controller('writerController', function($scope, $cookies) {
-    $scope.user = $cookies.get("user");
-    $scope.token = $cookies.get("token");
+        $scope.user = $cookies.get("user");
+        $scope.token = $cookies.get("token");
 
 
-    if (($scope.phone == "" || $scope.token == "") || ($scope.phone == undefined || $scope.token == undefined)) {
-        $scope.message = "Not LoggedIn";
-    } else {
-        $scope.message = "LoggedIn";
-    }
+        console.log($scope.user, $scope.token, "salam");
+
+
+        if ($scope.user == "" || $scope.token == "") {
+            console.log('user Not Logged In');
+            $scope.loggedIn = false;
+            $scope.quillDataJSON = "Please login to write a story";
+            $scope.quillDataText = "Please login to write a story";
+            $scope.quillDataHTML = "Please login to write a story";
+        } else {
+            console.log('user Logged In');
+            $scope.loggedIn = true;
+            $scope.quillDataJSON = "Preview will be displayed here";
+            $scope.quillDataText = "Preview will be displayed here";
+            $scope.quillDataHTML = "Preview will be displayed here";
+        }
+
+
+
+        // $scope.quillDataJSON = "Preview will be displayed here";
+        // $scope.quillDataText = "Preview will be displayed here";
+        // $scope.quillDataHTML = "Preview will be displayed here";
+
+        $scope.quillData = "hahaha";
+        $scope.quillConfig = "hahaConfig";
+
+        $scope.changeData = function() {
+            $scope.quillData = "config";
+        };
+
+        $scope.clickMe = function() {
+            alert("thanks!");
+        };
+    })
+    .directive('quillEditor', function($compile) {
+        return {
+            restrict: 'E',
+            link: function($scope, $element) {
+                var template = '<div id="editor">' +
+                    '<p>Hello World!</p>' +
+                    '<p>Some initial <strong>bold</strong> text</p>' +
+                    '<p><br></p>'
+                '</div>';
+                var linkFunc = $compile(template);
+                var content = linkFunc($scope);
+                $element.append(content);
+
+                // setup quill config after adding to DOM
+                var quill = new Quill('#editor', {
+                    modules: {
+                        // ImageResize: {},
+                        toolbar: [
+                            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                            // [{ 'header': 1 }, { 'header': 2 }],
+                            [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+                            ['bold', 'italic', 'underline', 'strike', 'link'],
+                            [{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+                            [{ 'font': [] }],
+                            [{ 'align': [] }],
+                            ['clean'], // remove formatting button
+                            ['blockquote', 'code-block'],
+                            ['video', 'image'],
+                            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                            [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
+                            [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+                        ]
+                    },
+                    placeholder: 'Compose an epic...',
+                    theme: 'snow' // or 'bubble'
+                });
+
+                quill.on('text-change', function() {
+                    var delta = quill.getContents();
+                    var text = quill.getText();
+                    var justHtml = quill.root.innerHTML;
+
+                    console.log(JSON.stringify(delta));
+
+                    $scope.$apply(function() {
+                        console.log("ha");
+                        console.log($scope);
+                        $scope.quillDataJSON = JSON.stringify(delta);
+                        $scope.quillDataText = text;
+                        $scope.quillDataHTML = justHtml;
+                    });
+                });
+            },
+        };
+
+
+
+    })
+
+
+helloModule.controller('registerController', function($scope) {
 
 })
 
